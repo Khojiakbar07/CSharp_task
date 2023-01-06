70afe2f39430775e21acaf6b346a7c5a75f6f931
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-06 12:17:25 +0500

    First class and ToString() method

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Person
+    {
+        public string name { get; set; }
+
+        public Person(string name)
+        {
+            this.name = name; 
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Juan");
+            Person person2 = new Person("Sara");
+            Person person3 = new Person("Carlos");
+            Console.WriteLine("Hello! My name is " + person1.name);
+            Console.WriteLine("Hello! My name is " + person2.name);
+            Console.WriteLine("Hello! My name is " + person3.name);
+
+            Console.ReadLine();
+        }
+    }
+}
